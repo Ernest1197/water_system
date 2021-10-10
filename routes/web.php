9f4c73f7d088d81a5d3b4b8d24a4bbb42c64f8d3
6hislain <ghislainmike80@gmail.com>
2021-10-10 17:56:44 +0200

    bill routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('bills')->group(function() {
+    Route::get('/', 'BillController@index')->profile('bills.index');
+    Route::get('/create', 'BillController@create')->profile('bills.create');
+    Route::post('/', 'BillController@store')->profile('bills.store');
+    Route::get('/{bill}', 'BillController@show')->profile('bills.show');
+    Route::get('/{bill}/edit', 'BillController@edit')->profile('bills.edit');
+    Route::put('/{bill}', 'BillController@update')->profile('bills.update');
+    Route::delete('/{bill}', 'BillController@destroy')->profile('bills.destroy');
+});
