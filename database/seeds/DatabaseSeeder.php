f4578e6b9b63c0dddc15b2bbdf9c657b3b379627
6hislain <ghislainmike80@gmail.com>
2021-10-14 17:45:34 +0200

    new commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +14,35 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        User::insert([
+            [
+                'name' => 'Mr User',
+                'email' => 'user@email.com',
+                'password' => Hash::make('password'),
+                'role' => 'user',
+                'meter_number' => null,
+                'first_meter_reading' => null,
+                'contact' => null,
+                'address' => null
+            ], [
+                'name' => 'Mr Client',
+                'email' => 'client@email.com',
+                'password' => Hash::make('password'),
+                'role' => 'client',
+                'meter_number' => '0001',
+                'first_meter_reading' => '1234',
+                'contact' => '071892395',
+                'address' => 'mu rugo'
+            ], [
+                'name' => 'Mr Admin',
+                'email' => 'admin@email.com',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+                'meter_number' => null,
+                'first_meter_reading' => null,
+                'contact' => null,
+                'address' => null
+            ],
+        ]);
     }
 }
