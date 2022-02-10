4c43c9754030fb63613d6562b01cfba0df53b973
6hislain <ghislainmike80@gmail.com>
2022-02-10 13:53:11 +0200

    new commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,8 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
- | Mix Asset Management
+ | Mix secure_asset Management
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
@@ -11,5 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js("resources/js/app.js", "public/js").sass(
+    "resources/sass/app.scss",
+    "public/css"
+);
